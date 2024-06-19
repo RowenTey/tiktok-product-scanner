a70a60b07b5af4b6d37fcbb9f4c8b1538680fc06
Lee Juin <juin.lee@outlook.com>
2024-06-19 15:19:48 +0800

    added way to extract labels + confidence from yolo prediction

--- object-detection/main.py
+++
@@ -1,8 +0,0 @@
-from ultralytics import YOLO
-
-# Load a model
-## model = YOLO("yolov8n.yaml")  # build a new model from scratch
-model = YOLO("yolov8x-cls.pt")  # load a pretrained model (recommended for training)
-
-# Use the model
-path = model.export(format="coreml")  # export the model to CoreML format
